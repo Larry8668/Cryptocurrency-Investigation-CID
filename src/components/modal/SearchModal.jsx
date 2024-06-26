aa34c29fd54fc5b535479f946a638ea7964035d1
Larry8668 <114809719+Larry8668@users.noreply.github.com>
2024-06-26 10:29:02 +0530

    added tooltips to buttons

--- src/components/modal/SearchModal.jsx
+++ src/components/modal/SearchModal.jsx
@@ -3,13 +3,13 @@ import { Link, useNavigate } from "react-router-dom";
 import { IoCloseCircleOutline } from "react-icons/io5";
 import { FiExternalLink } from "react-icons/fi";
 import { IoMdSearch } from "react-icons/io";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 
 const SearchModal = ({ isModalOpen, setIsModalOpen }) => {
   const [search, setSearch] = useState("");
   const navigate = useNavigate();
   const handleSearch = () => {
     if (search.length === 0) return;
-    console.log("routing?")
     navigate(`/elkjs/${search}`);
   };
   return (
@@ -20,6 +20,8 @@ const SearchModal = ({ isModalOpen, setIsModalOpen }) => {
             <button
               className="p-1 border-2 border-slate-400 bg-transparent rounded-md text-red-600"
               onClick={() => setIsModalOpen(false)}
+              data-tooltip-id={"Close Button"}
+              data-tooltip-content={"Close"}
             >
               <IoCloseCircleOutline />
             </button>
@@ -32,12 +34,59 @@ const SearchModal = ({ isModalOpen, setIsModalOpen }) => {
             <button
               className="p-2 bg-transparent border-2 border-slate-400 rounded-md"
               onClick={() => handleSearch()}
+              data-tooltip-id={"Search Button"}
+              data-tooltip-content={"Search"}
             >
               <IoMdSearch />
             </button>
-            <Link className="p-2 bg-transparent text-black border-2 border-slate-400 rounded-md" to={`/elkjs/${search}`} target="_blank">
+            <Link
+              className="p-2 bg-transparent text-black border-2 border-slate-400 rounded-md"
+              to={`/elkjs/${search}`}
+              target="_blank"
+              data-tooltip-id={"Link Button"}
+              data-tooltip-content={"Open in New Tab"}
+            >
               <FiExternalLink />
             </Link>
+            <ReactTooltip
+              id={"Close Button"}
+              place="bottom"
+              effect="solid"
+              style={{
+                backgroundColor: "rgba(0, 0, 0, 0.85)",
+                color: "#fff",
+                padding: "4px 8px",
+                borderRadius: "4px",
+                fontSize: "12px",
+                boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
+              }}
+            />
+            <ReactTooltip
+              id={"Search Button"}
+              place="bottom"
+              effect="solid"
+              style={{
+                backgroundColor: "rgba(0, 0, 0, 0.85)",
+                color: "#fff",
+                padding: "4px 8px",
+                borderRadius: "4px",
+                fontSize: "12px",
+                boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
+              }}
+            />
+            <ReactTooltip
+              id={"Link Button"}
+              place="bottom"
+              effect="solid"
+              style={{
+                backgroundColor: "rgba(0, 0, 0, 0.85)",
+                color: "#fff",
+                padding: "4px 8px",
+                borderRadius: "4px",
+                fontSize: "12px",
+                boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
+              }}
+            />
           </div>
         </div>
       )}
