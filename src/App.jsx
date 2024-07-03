f1ff53b9d1441bd24a327d4cd1dcb96a58ba6860
pramaths <103276292+pramaths@users.noreply.github.com>
2024-07-03 11:35:00 +0530

    strucutre revamp

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
-import React, { useState } from "react";
-import { title } from "../ui/title";
-import { subtitle } from "../ui/subtitle";
-import GraphComponent from "./components/Graph";
-import Search from "./Search";
+import ReactDOM from "react-dom/client";
+import ErrorPage from "./error-page";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { NextUIProvider } from "@nextui-org/react";
+import LandingPageComponent from "./components/LandingPageComponent.jsx";
+import { ElkPage } from "./elkjs/ElkPage.jsx";
+import { ReactFlowProvider } from "reactflow";
+import { GlobalProvider } from "./context/GlobalContext";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPageComponent />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/elkjs",
+    element: <ElkPage />,
+  },
+  {
+    path: "elkjs/:centralNodeAddress",
+    element: <ElkPage />,
+  },
+]);
 
-export default function App() {
-  return (
-    <div className=" w-[100vw] bg-black bg-grid-white/[0.2] relative flex flex-col items-center justify-center py-16">
-      <div
-        style={{
-          background:
-            "radial-gradient(circle at center, rgba(0,0,0,0) 0%, rgba(0,0,0,0.7) 30%, rgba(0,0,0,1) 100%)",
-        }}
-        className="absolute pointer-events-none inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
-      ></div>
-      <div className="w-full h-full flex flex-col justify-center text-center md:justify-start items-center">
-      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-        Enter Your Sender Address Here:
-      </p>
-      <Search />
-      </div>
-    </div>
-    // <div className='flex flex-col items-center  h-[100vh] w-[100vw] py-4 gap-6 '>
-    // <h1 className={`${title()} m-4`}>
-    //   Enter Your Sender Address Here:
-    // </h1>
-    // <Search />
-    // </div>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <GlobalProvider>
+    <NextUIProvider>
+      <ReactFlowProvider>
+        <RouterProvider router={router} />
+      </ReactFlowProvider>
+    </NextUIProvider>
+  </GlobalProvider>
+);
