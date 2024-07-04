0473c94fcd53b40f952a2810b381610e2d365d3f
Larry8668 <114809719+Larry8668@users.noreply.github.com>
2024-07-04 16:40:38 +0530

    fixing env imports

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  plugins: [react()],
 
-plugins: [react()],
+  define: {
+    "process.env.VITE_VERCEL_DEV_MODE": JSON.stringify(
+      process.env.VITE_VERCEL_DEV_MODE
+    ),
 
-define: {
+    "process.env.VITE_VERCEL_KV_REST_API_READ_ONLY_TOKEN": JSON.stringify(
+      process.env.VITE_VERCEL_KV_REST_API_READ_ONLY_TOKEN
+    ),
+    "import.meta.env.VITE_VERCEL_KV_REST_API_URL": JSON.stringify(
+      process.env.VITE_VERCEL_KV_REST_API_URL
+    ),
+    "import.meta.env.VITE_VERCEL_KV_REST_API_TOKEN": JSON.stringify(
+      process.env.VITE_VERCEL_KV_REST_API_TOKEN
+    ),
 
-'process.env.VITE_VERCEL_DEV_MODE':JSON.stringify(process.env.VITE_VERCEL_DEV_MODE),
-
-'process.env.VITE_VERCEL_KV_REST_API_READ_ONLY_TOKEN':JSON.stringify(process.env.VITE_VERCEL_KV_REST_API_READ_ONLY_TOKEN),
-
-'process.env.VITE_VERCEL_KV_REST_API_TOKEN':JSON.stringify(process.env.VITE_VERCEL_KV_REST_API_TOKEN),
-
-'process.env.VITE_VERCEL_KV_REST_API_URL':JSON.stringify(process.env.VITE_VERCEL_KV_REST_API_URL),
-
-'process.env.VITE_VERCEL_KV_URL':JSON.stringify(process.env.VITE_VERCEL_KV_URL),
-
-}
-
-})
+    "process.env.VITE_VERCEL_KV_URL": JSON.stringify(
+      process.env.VITE_VERCEL_KV_URL
+    ),
+  },
+});
